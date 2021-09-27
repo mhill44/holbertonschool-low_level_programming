d065b264acba94ccf803d4f3ffe938a12b2aa498
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-09-27 15:44:30 -0500

    Add files via upload
    mhill44

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h
+
+int main (void)
+{
+       print_alphabet();
+       return (0);
+}
+//int main()
+//char x;
+//for (x = 'A'; x <= 'Z'; x++)
+//{
+//printf("%x ", x);
+//return (0);
+//}
+       
+       // it shoud be as simple as printf_alphabet > (arrayAlphabet[]) to put them in an array form output. i dont understand why the loop isnt working
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,3 @@
-Matt H working through 0x02 Nested_loops assignment
+this is matt H doing project 0x02. this is the readme file
+// ** i have a massive new understanding after reading the betty wiki and revisiting everthing. i am tring to redo every bit of code ive written thus far. THIS IS INDEPENDANCE DAY WEEKEND / MONDAY. i finally got the help i needed and it all clicked *** ///
+
