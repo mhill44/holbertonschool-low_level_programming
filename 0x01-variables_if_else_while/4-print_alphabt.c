9cee5f2afd1f22a44bc31ac83431a224b8e88587
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-09-28 22:46:03 -0500

    Update 4-print_alphabt.c
    mhill44

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-    char a;
+char a;
 
-    for (a = 'a'; a <= 'z' ; a++)
-    {
-        if (a != 'e' && a != 'q')
-            putchar(a);
-    }
-    putchar('\n');
-    return (0);
+for (a = 'a'; a <= 'z' ; a++)
+{
+if (a != 'e' && a != 'q')
+putchar(a);
+}
+putchar('\n');
+return (0);
 }
