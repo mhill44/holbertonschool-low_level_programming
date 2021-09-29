d4f25ce25480506aa3b250636be2236f1a436345
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-09-29 15:42:23 -0500

    Update 1-alphabet.c
    mhill44

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <stddef.h>
+#include <main.h>
 #include <stdlib.h>
-
-int main (void)
+#include <stdio.h>
+/*
+* Return: lowercase alphabet in line from a-z
+*/
+void print_alphabet(void)
 {
-       print_alphabet();
-       return (0);
+    char c;
+    c = 'a'
+while (c <= 'z'){
+    _putchar (c);
+    c++;
 }
-//int main()
-//char x;
-//for (x = 'A'; x <= 'Z'; x++)
-//{
-//printf("%x ", x);
-//return (0);
-//}
-       
-       // it shoud be as simple as printf_alphabet > (arrayAlphabet[]) to put them in an array form output. i dont understand why the loop isnt working
+_putchar('n');
+}
