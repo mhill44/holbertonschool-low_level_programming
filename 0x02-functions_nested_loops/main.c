de496842667ab49f263a6329a52b40314b9bc607
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-09-29 22:54:43 -0500

    Update main.c
    mhill44

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,4 +1,15 @@
+#include "holberton.h"
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
 int _putchar(char c);
 void print_alphabet (void);
 void print_alphabet_x10(void);
