23f4a71fe068f35abf14c1787cc57ee0212279f2
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-09-29 23:09:16 -0500

    Add files via upload
    mhill44

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+include "holberton.h"
+include "main.h"
+
+int main()
+{
+    char az;
+
+    printf("Alphabets from a - z are: \n");
+    for(az ='a'; az <='z'; az++)
+    {
+        printf("%c\n", ch);
+    }
+
+    return 0;
+}
