2932537fff18da7b525930cc202f2d4a9b432d4d
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-09-30 23:34:26 -0500

    Update main.h
    mhill44

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,24 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include "main.h"
+/*#
+* void positive_or_negative(int i);
+* #endif HOLBERTON_H
+*/
 int _putchar(char c);
 void print_alphabet (void);
 void print_alphabet_x10(void);
 int islower(int c);
 int isalpha(int c);
 int print_sign(int n);
-int _abs(int);
 int print_last_digit(int);
 void jack_bauer (void);
 void times_table (void);
 int add (int, int);
 void print_to_98 (int n);
+
+#endif // HOLBERTON_H_INCLUDED
