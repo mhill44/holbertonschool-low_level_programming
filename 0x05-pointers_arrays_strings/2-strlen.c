a6ab8932d5de0a2ffac722373518e0f9555ce891
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-10-06 16:53:17 -0500

    Add files via upload
    mhill44

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-/**
+/*
  * _strlen - length of a string
  * *s: the string to check
  * Return: length of the string
@@ -11,12 +11,10 @@
 int _strlen(char *s)
 {
 int length;
-
- length = 0;
-
- for (length = 0; *s != '\0'; s++)
- {
- length++;
- }
+length = 0;
+for (length = 0; *s != '\0'; s++)
+{
+length++;
+}
 return (length);
 }
