58e5ec6aa2dac1aa6abdf9060c963d611287d566
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-10-08 00:03:54 -0500

    Add files via upload
    mhill44

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * main - Print numbers from 1 to 100 "Fizz-Buzz test"
+ * Description: Print numbers from 1 to 100, replacing multiples of 3 with Fizz
+ * and for multiples of 5, print Buzz. Multiples of both 3 and five print Fizz
+ * Buzz
+ * Return: even though void, return 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+if (i < 100)
+printf(" ");
+else
+printf("\n");
+ }
+ return (0);
+}
