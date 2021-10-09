5b653d35566728d8592a6c7aebbc7c76d0c4c5bf
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-10-09 01:29:56 -0500

    Add files via upload
    mhill44 mass upload due to renaming folder

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _strncat - concatenates two strings
+ * @src: string 1
+ * @dest: string 2
+ * @n: is the number of bytes in src
+ * Return: returns a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int l;
+l = _strlen(dest);
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[l + i] = src[i];
+dest[l + i] = '\0';
+return (dest);
+}
+/**
+* _strlen - length of a string
+* @s: the string to check
+* Return: length of the string
+*/
+int _strlen(char *s)
+{
+int length;
+length = 0;
+for (length = 0; *s != '\0'; s++)
+{
+length++;
+}
+return (length);
+}
