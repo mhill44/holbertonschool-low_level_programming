22e2b425ea8cc89b36744b1b7b2570a1581041c2
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-10-11 16:00:41 -0500

    Update 1-strncat.c
    mhill44

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,26 +4,9 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
-* _strncat - concatenates two strings
-* @src: string 1
-* @dest: string 2
-* @n: is the number of bytes in src
-* Return: returns a pointer to the resulting string dest
-*/
-char *_strncat(char *dest, char *src, int n)
-{
-int i;
-int l;
-l = _strlen(dest);
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[l + i] = src[i];
-dest[l + i] = '\0';
-return (dest);
-}
-/**
 * _strlen - length of a string
 * @s: the string to check
-* Return: length of the string
+* Return: returns the length of the string
 */
 int _strlen(char *s)
 {
@@ -34,4 +17,21 @@ for (length = 0; *s != '\0'; s++)
 length++;
 }
 return (length);
+}
+/**
+* _strncat: function appends two strings together
+* @src: string 1
+* @dest: string 2
+* @n: is the number of bytes in src
+* Return: returns a pointer to the new string dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int l;
+l = _strlen(dest);
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[l + i] = src[i];
+dest[l + i] = '\0';
+return (dest);
 }
