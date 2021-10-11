7b5d512177c65f138ea3c18a7ba8ad1c8f1cc9ff
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-10-11 13:01:59 -0500

    Add files via upload
    mhill44 removing whitespaces

---
+++ 0-strcat.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+* _strcat - concatenates two strings
+* @src: the first string
+* @dest: the second string
+* Return: returns the dest string
+*/
+char *_strcat(char *dest, char *src)
+{
+int len = _strlen(dest);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+dest[len + i] = src[i];
+dest[len + i] = '\0';
+return (dest);
+}
+/**
+* _strlen - length of a string
+* @s: the string to check
+* Return: returns length of the string
+*/
+int _strlen(char *s)
+{
+int length;
+length = 0;
+for (length = 0; *s != '\0'; s++)
+{
+length++;
+}
+return (length);
+}
