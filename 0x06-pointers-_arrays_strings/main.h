d5b37a390729b2e9710d769fde2bca97058ddae7
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-10-11 16:20:02 -0500

    Update main.h
    mhill44

--- 0x06-pointers-_arrays_strings/main.h
+++ 0x06-pointers-_arrays_strings/main.h
@@ -1,9 +1,8 @@
-void more_numbers(void);
-s#define HOLBERTON_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <stdio.h>
-#include <stddef.h>
-/*#
-* void positive_or_negative(int i);
+#include "holberton.h"
+/**
 * #endif HOLBERTON_H
 */
 int _putchar(char c);
@@ -15,6 +14,7 @@ int print_sign(int n);
 int print_last_digit(int);
 void jack_bauer (void);
 void times_table (void);
+void positive_or_negative(int i);
 int add (int, int);
 void print_to_98 (int n);
 void reset_to_98(int *n);
@@ -26,6 +26,7 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _isdigit(int c);
 int mul(int a, int b);
+void more_numbers(void);
 void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int n);
@@ -36,6 +37,8 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-void reverse_array(
+void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+
+#endif
