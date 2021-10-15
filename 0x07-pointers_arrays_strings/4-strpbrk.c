71381a219a6f937869a552c708f46c00c4cef106
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-10-15 13:37:02 -0500

    Update 4-strpbrk.c
    mhill44

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 #include "main.h"
 /**
-* _strpbrk - function that locates the first occurrence in the string s
-* of any of the bytes in the string accept
+* _strpbrk - function that locates the first occurrence in
+* the string sof any of the bytes in the string accept
 * @s: is the string to search
 * @accept: is the bytes to search for
-* Return: returns a pointer to the byte in s or NULL if no adequate byte is found
+* Return: returns a pointer to the byte in s
+* or returns NULL if no adequate byte is found
 */
 char *_strpbrk(char *s, char *accept)
 {
