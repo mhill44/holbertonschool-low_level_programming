8d56baf9ebcd7af44837970d0312f9b12351631c
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-10-18 16:33:20 -0500

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+* _puts - is function that is custom for puts
+* void returns nothing
+*/
 int main(void)
 {
 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
