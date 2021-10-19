7d7d3468510e5de11d28efc856982d9531fd6f48
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-10-18 19:37:31 -0500

    Add files via upload
    mhill44

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include "holberton.h"
+/**
+* *_puts - is function that is custom for puts
+* return: void returns nothing
+*/
+int main(void)
+{
+_puts("\"At the end of the day, blah\"\n\t- Kevin Mitnick");
+return (0);
+}
