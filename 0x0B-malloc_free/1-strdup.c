063a95de24b91713fb3aeda0f880d62222c03114
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-10-20 23:40:02 -0500

    Add files via upload
    mhill44

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
+* _strlen - function that returns the length of a string
+* @s: is the string to check
+* Return: returns The length of the string
+*/
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+;
+return (i);
+}
+/**
 * _strdup - funct returns a pointer to a new string which is a duplicate of the
 * string str
 * @str: is the exact string to copy
@@ -20,16 +32,4 @@ for (i = 0; i < length; i++)
 copy[i] = str[i];
 copy[i] = '\0';
 return (copy);
-}
-/**
-* _strlen - function that returns the length of a string
-* @s: is the string to check
-* Return: returns The length of the string
-*/
-int _strlen(char *s)
-{
-int i;
-for (i = 0; s[i] != '\0'; i++)
-;
-return (i);
 }
