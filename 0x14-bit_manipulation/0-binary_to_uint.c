3c204c40472806694272d7543525dd76d38531d6
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-11-12 17:17:28 -0600

    Update 0-binary_to_uint.c
    mhill44

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "holberton.h"
+int _strlen(const char *s);
 /**
 * _strlen - this is a function that finds a length of a string
 * @s: this is the string to check
@@ -9,14 +10,13 @@ int _strlen(const char *s)
 {
 int i;
 for (i = 0; s[i] != '\0'; i++)
-;
 return (i);
 }
 /**
-* binary_to_uint - is a function that converts a binary number to u.s.
+* binary_to_uint - is a function that converts a binary number to an unsigned
 * @b: this is a pointer to a string of 0 and 1 chars
-* Return: returns the converted number, or 0 if there is one or more 
-* chars in the string b that is not 0 or 1 or if b is NULL
+* Return: returns converted number, or 0 if there is one or more chars in the
+* string b that is not 0 or 1 or if b is NULL
 */
 unsigned int binary_to_uint(const char *b)
 {
