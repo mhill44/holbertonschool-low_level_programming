9687d4e4f0e88c67f148fc7d6bdefd046ee6599f
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-11-12 23:14:51 -0600

    Add files via upload
    mhill44

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,39 +1,20 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
-* _strlen - this is a function that finds a length of a string
-* @s: this is the string to check
-* Return: returns the length of the string
-*/
-int _strlen(const char *s)
-{
-int i;
-for (i = 0; s[i] != '\0'; i++)
-return (i);
-}
-/**
-* binary_to_uint - is a function that converts a binary number to an unsigned
-* @b: this is a pointer to a string of 0 and 1 chars
-* Return: returns converted number, or 0 if there is one or more chars in the
-* string b that is not 0 or 1 or if b is NULL
+* binary_to_uint - function that converts a binary number to unsigned int
+* @b: is the string containing the binary number
+* Return: returns the converted number
 */
 unsigned int binary_to_uint(const char *b)
 {
-int len, i = 1;
-unsigned int uint = 0, base = 1;
-if (b == NULL)
+int i;
+unsigned int dec_val = 0;
+if (!b)
 return (0);
-len = _strlen(b);
-for (; i < len; i++)
+for (i = 0; b[i]; i++)
 {
-if (b[i] != '0' && b[i] != '1')
+if (b[i] < '0' || b[i] > '1')
 return (0);
+dec_val = 2 * dec_val + (b[i] - '0');
 }
-for (i = len - 1; i >= 0; i--)
-{
-if (b[i] == '1')
-uint += base;
-base *= 2;
-}
-return (uint);
+return (dec_val);
 }
