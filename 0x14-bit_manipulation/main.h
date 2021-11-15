af285b96773921f0ec5745ed2bafa44be74094dd
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-11-15 14:27:01 -0600

    Add files via upload
    mhill44 adding prototype to main.h file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H /* MAIN_H */
 
+int _putchar(char c);
 void print_binary(unsigned long int n);
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
