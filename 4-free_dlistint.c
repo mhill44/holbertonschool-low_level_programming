8b3c80506c2c8c6ad3756429d088b37525684cda
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-12-09 00:02:22 -0600

    Add files via upload
    mhill44

---
+++ 4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* free_dlistint - a function that frees the dlistint_t list
+* @head: is the pointer
+* Return: return returns nothing
+*/
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *tmp;
+if (head == NULL)
+return;
+while (head)
+{
+tmp = head;
+head = head->next;
+free(tmp);
+}
+head = NULL;
+}
