515d5ea311de13cf8448bc72f11f2cd295ffe38b
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-12-09 17:50:24 -0600

    Add files via upload
    mhill44 mass upload

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+/**
+* hash_table_create - function creates a hash table
+* @size: is size of the array of the hash table
+* Return: Returns the Pointer to the newly created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *ht;
+if (size == 0)
+return (NULL);
+ht = malloc(sizeof(hash_table_t));
+if (!ht)
+return (NULL);
+ht->size = size;
+ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+if (ht->array == NULL)
+return (NULL);
+return (ht);
+}
---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+* hash_djb2 - is a hash function implementing the djb2 algorithm
+* @str: is the string to hash
+* Return: returns the hash index corresponding to the string
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash;
+int c;
+hash = 5381;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+}
+return (hash);
+}
