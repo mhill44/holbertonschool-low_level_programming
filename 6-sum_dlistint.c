aa5c11d4d1c3debc3b57437dd9b977c98a1f80c4
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-12-09 00:13:22 -0600

    Add files via upload
    mhill44

---
+++ 6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* sum_dlistint - a function that returns the sum of all the data
+* @head: is the head the pointer
+* Return: turuns the data (n) or if the list is empty, return 0
+*/
+int sum_dlistint(dlistint_t *head)
+{
+int sum;
+dlistint_t *current;
+sum = 0;
+current = head;
+while (current)
+{
+sum += current->n;
+current = current->next;
+}
+return (sum);
+}
