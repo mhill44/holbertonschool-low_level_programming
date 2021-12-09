c32822c823ca71858c4415751ae58f066579b2da
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-12-09 00:39:48 -0600

    Add files via upload
    mhill44

--- 0x18-doubly_linked_lists/9-print_dlistint_backward.c
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -1,1 +1,36 @@
-/**placeholder*/
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* delete_dnodeint_at_index - a function that deletes the node at index
+* of a dlistint_t linked list
+* @head: double pointer to the head of the list
+* @index: is the index of the node to delete
+* Return: returns 1 for success, -1 for failure
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+dlistint_t *current = *head;
+unsigned int i = 0;
+if (*head == NULL || dlistint_len(current) < index + 1)
+return (-1);
+if (!index)
+{
+(*head) = current->next;
+if (current->next)
+current->next->prev = NULL;
+current->next = NULL;
+free(current);
+return (1);
+}
+while (i < index)
+{
+current = current->next;
+i++;
+}
+current->prev->next = current->next;
+if (current->next)
+current->next->prev = current->prev;
+free(current);
+return (1);
+}
