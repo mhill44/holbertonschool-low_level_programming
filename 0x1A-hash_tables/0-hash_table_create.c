1a48bb53a36215d27ef06185f4a7a47499f8c6b5
mhill44 <90353090+mhill44@users.noreply.github.com>
2021-12-13 14:28:52 -0600

    Update 0-hash_table_create.c
    mhill44

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,13 @@
 #include "hash_tables.h"
 /**
-* hash_table_create - function creates a hash table
+* hash_table_create - this function creates a hash table
 * @size: is size of the array of the hash table
 * Return: Returns the Pointer to the newly created hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
+{
 hash_table_t *ht;
+
 if (size == 0)
 return (NULL);
 ht = malloc(sizeof(hash_table_t));
