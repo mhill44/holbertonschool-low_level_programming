89ca378354f4c8c2af31daea9a6aac84d563c4ed
mhill44 <3452@holbertonschool.com>
2022-06-19 16:07:54 -0500

    fixing syntax

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - search  an array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number element in array
+ * @value: is value for search
+ * Return: return the first index where value is located or -1 if is none
+ * if array is NULL return -1
+ * Every time you compare a value in the array to the value you are searching
+ * you have to print this value
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t h = size - 1, l = 0, i = 0;
+	int m;
+
+	if (array == NULL)
+		return (-1);
+	while (l <= h)
+	{
+		m = l + ((h - l) / 2);
+		printf("Searching in array: ");
+		for (i = l; i <= h; i++)
+		{
+			if (i < h)
+				printf("%d, ", array[i]);
+			else
+				printf("%d\n", array[i]);
+		}
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			h = m - 1;
+		else
+			return (m);
+	}
+	return (-1);
+}
