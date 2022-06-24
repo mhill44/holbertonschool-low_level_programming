56fe5288b1ea125fd6a8d97ec159433aaf25425f
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-06-23 23:51:56 -0500

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int aaa(int *array, size_t size, int value, int l, int h);
+int advanced_binary(int *array, size_t size, int value)
 
 
 
